c43c57f15572f5450260dabc7cabc7ea82abc422
Lucio Pinese <github.913zc@simplelogin.com>
2024-10-29 17:21:09 +0100

    doing shit...

--- src/widgets/new_order_widget.rs
+++ src/widgets/new_order_widget.rs
@@ -1,12 +1,11 @@
 use ratatui::{
-  buffer::Buffer,
-  layout::{Constraint, Direction, Flex, Layout, Rect},
-  style::{Color, Style},
-  widgets::{Block, Widget},
+  buffer::Buffer, layout::{Constraint, Direction, Flex, Layout, Rect}, style::{Color, Style, Stylize}, text::{self, Text}, widgets::{Block, Borders, List, ListDirection, Paragraph, Widget}
 };
 use nostr_sdk::prelude::*;
 use std::str::FromStr;
 use mostro_core::order::Order;
+
+use crate::popup_area;
 
 
 #[derive(Debug,Default)]
@@ -22,7 +21,7 @@ impl NewOrderWidget {
 
 impl Widget for NewOrderWidget {
   fn render(self, area: Rect, buf: &mut Buffer) {
-      let layout = create_layout(area,50,50);
+      let layout = create_layout(area,80,60);
       render_block(
           layout,
           buf,
@@ -32,12 +31,36 @@ impl Widget for NewOrderWidget {
   }
 }
 
+// ANCHOR: centered_rect
+/// helper function to create a centered rect using up certain percentage of the available rect `r`
+fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+  // Cut the given rectangle into three vertical pieces
+  let popup_layout = Layout::default()
+      .direction(Direction::Vertical)
+      .constraints([
+          Constraint::Percentage((100 - percent_y) / 2),
+          Constraint::Percentage(percent_y),
+          Constraint::Percentage((100 - percent_y) / 2),
+      ])
+      .split(r);
+
+  // Then cut the middle vertical piece into three width-wise pieces
+  Layout::default()
+      .direction(Direction::Horizontal)
+      .constraints([
+          Constraint::Percentage((100 - percent_x) / 2),
+          Constraint::Percentage(percent_x),
+          Constraint::Percentage((100 - percent_x) / 2),
+      ])
+      .split(popup_layout[1])[1] // Return the middle chunk
+}
+
   /// helper function to create a centered rect using up certain percentage of the available rect `r`
 fn create_layout(area: Rect, percent_x: u16, percent_y: u16) -> Rect {
-  let vertical = Layout::vertical([Constraint::Percentage(percent_y)]).flex(Flex::Center);
-  let horizontal = Layout::horizontal([Constraint::Percentage(percent_x)]).flex(Flex::Center);
-  let [area] = vertical.areas(area);
-  let [area] = horizontal.areas(area);
+   let area = centered_rect(percent_x, percent_y, area);
+        //frame.render_widget(popup_block, area);
+        // ANCHOR_END: editing_popup
+   
   area
 }
 
@@ -45,6 +68,35 @@ fn create_layout(area: Rect, percent_x: u16, percent_y: u16) -> Rect {
 fn render_block(area: Rect, buf: &mut Buffer, title: &str, label: &str) {
   let block = Block::bordered().title(title);
   let inner_area = block.inner(area);
+
+  let popup_chunks = Layout::default()
+  .direction(Direction::Horizontal)
+  .margin(1)
+  .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+  .split(inner_area);   
+
+  let items = ["Kind (Sell/Buy)", "Currency", "Amount","Payment method","Premium"];
+  let list = List::new(items)
+    .block(Block::bordered().title("Oreder parameter"))
+    .style(Style::new().white())
+    .highlight_style(Style::new().italic())
+    .highlight_symbol(">>")
+    .repeat_highlight_symbol(true)
+    .direction(ListDirection::TopToBottom);
+  
+
+  // ANCHOR: key_value_blocks
+  // let key_block = Block::default().title("Order Kind").borders(Borders::ALL);
+  // let value_block = Block::default().title("Amount").borders(Borders::ALL);
+  // let currency_block = Block::default().title("Currency").borders(Borders::ALL);
+
+  // let key_text = Paragraph::new(Text::styled("Hello, world!", Style::default())).block(key_block);
+  // // 
+  list.render(popup_chunks[0],buf);
+  // value_block.render(popup_chunks[0],buf);
+  // currency_block.render(popup_chunks[0],buf);
+
+
   block.render(area, buf);
 
   render_label_and_value(inner_area, buf, label);
